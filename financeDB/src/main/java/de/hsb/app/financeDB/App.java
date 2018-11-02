09633c71ed9eb511fcdc3f48ea186d35dd73fd38
marce <marce@DESKTOP-VKKNF6G.multi.box>
2018-11-02 11:41:12 +0100

    first

---
+++ financeDB/src/main/java/de/hsb/app/financeDB/App.java
@@ -0,0 +1,13 @@
+package de.hsb.app.financeDB;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
